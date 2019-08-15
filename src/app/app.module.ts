80abb9a1e0917c6c7e82914761a94662f8e45a23
Luciano Closs <lcloss@gmail.com>
2019-08-15 18:59:13 +0100

    Before run npm audit fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule, LOCALE_ID } from '@angular/core'
 import { HttpModule } from '@angular/http'
-import { RouterModule } from '@angular/router'
+import { RouterModule, PreloadAllModules } from '@angular/router'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
@@ -19,7 +19,6 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 import { SharedModule } from './shared/shared.module'
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -38,9 +37,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
-    CoreModule,
-    SharedModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-PT'}],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { InputComponent } from './input/input.component'
-import { RadioComponent } from './radio/radio.component'
-import { RatingComponent } from './rating/rating.component'
+import { InputComponent } from './input/input.component';
+import { RadioComponent } from './radio/radio.component';
+import { RatingComponent } from './rating/rating.component';
+
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { OrderService } from 'app/order/order.service';
 
 @NgModule({
     declarations: [
@@ -28,5 +32,15 @@ import { RatingComponent } from './rating/rating.component'
 })
 
 export class SharedModule { 
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [
+                ShoppingCartService,
+                RestaurantsService,
+                OrderService
+            ]
+        }
+    }
     
 }
